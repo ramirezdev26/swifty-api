aeddb9cf8814b725c91efed07fce9721a35c60b7
OwenMalo <owen.sanchez@jala.university>
2025-10-05 10:55:47 -0600

    feat: updating full pipeline with index and logger

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+import pino from 'pino';
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname',
+    },
+  },
+});
+
+// Start the application
+logger.info('Application started');
