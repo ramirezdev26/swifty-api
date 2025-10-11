7d03fdd427cc04ce6eba405982501c481f37afbc
JuanD_LG <Juan.Lemus@jala.university>
2025-10-11 18:47:12 -0500

    feat(auth): add config to database

--- src/infrastructure/config/env.js
+++ src/infrastructure/config/env.js
@@ -10,6 +10,13 @@ export const config = {
     frontendUrl: process.env.FRONTEND_URL,
     apiPrefix: process.env.API_PREFIX,
   },
+  database: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
   firebase: {
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
